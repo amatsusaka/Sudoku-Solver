826a873c4d4acf36864df462f7d88c0b2865cc35
Russ <russplaysguitar@yahoo.com>
2011-08-09 23:19:35 -0700

    Reapplied fixes made by Matt earlier...

--- SudokuSolve.py
+++ SudokuSolve.py
@@ -82,11 +82,11 @@ class Solver:
 	def BlockToArray( self, grid, blockNum ):
 		xcoord = 0
 		ycoord = 0
-		gridSq = sqrt( len( grid ) )
+		gridSq = int(sqrt( len( grid ) ))
 		array = []
 		
-		ycoord = blockNum // gridSq * gridSq
-		xcoord = (blockNum % gridSq) * gridSq
+		ycoord = int(blockNum // gridSq) * gridSq
+		xcoord = int(blockNum % gridSq) * gridSq
 		
 		for y in range(ycoord, ycoord+gridSq):
 			for x in range(xcoord, xcoord+gridSq):
@@ -229,129 +229,6 @@ class Solver:
 		psbGrid = copy.deepcopy( undoGrid )
 		grid[x][y] = 0
 		return False
-	
-	
-########################################
-#			Test Cases
-########################################
-def TestCheckArray():
-	#print "Testing array check..."
-	a = [4, 8, 9, 2, 1, 3] #should pass
-	b = [8, 8, 2, 1, 3, 9] #should fail
-	
-	failed = True
-	
-	if CheckArray( a ) == True:
-		failed = False
-	else:
-		failed = True
-		
-	if CheckArray( b ) == False:
-		failed = False
-	else:
-		failed = True
-		
-	return failed
-
-def TestColToArray():
-	#print "Testing column to array..."
-	a = [
-		[1, 2, 3, 4, 5],
-		[6, 7, 8, 9, 10],
-		[11, 12, 13, 14, 15],
-		[16, 17, 18, 19, 20],
-		[21, 22, 23, 24, 25]
-		]
-	checkArray = [1, 6, 11, 16, 21]
-	
-	failed = True
-	
-	array = ColToArray( a, 0 )
-	
-	if array == checkArray:
-		failed = False
-		
-	return failed
-
-def TestCheckGridDimensions():
-	#print "Testing grid validation..."
-	a = [
-		[1, 2, 3, 4, 5],
-		[6, 7, 8, 9, 10],
-		[11, 12, 13, 14, 15],
-		[16, 17, 18, 19, 20],
-		[21, 22, 23, 24, 25]
-		]
-		
-	failed = True
-	
-	valid = CheckGridDimensions( a )
-	if valid == False:
-		failed = False
-		
-	return failed
-	
-def TestBlockToArray():
-	#print "Testing blcok to array..."
-	a = [
-		[1, 2, 3, 4],
-		[5, 6, 7, 8],
-		[9, 10, 11, 12],
-		[13, 14, 15, 16]
-		]
-	checkArray = [3, 4, 7, 8]
-	
-	failed = True
-	
-	array = BlockToArray( a, 1 )
-	
-	if array == checkArray:
-		failed = False
-		
-	return failed
-	
-def TestLocateNextEmpty():
-	failed = True
-	a = [
-		[1, 2, 3, 4],
-		[5, 6, 7, 8],
-		[9, 10, 0, 12],
-		[13, 14, 15, 16]
-		]
-	val = LocateNextEmpty( a )
-	if val == [2,2]:
-		failed = False
-	return failed
-	
-def TestLocateBlock():
-	failed = True
-	grid = ReadGridFromFile('TestCase1.txt')
-	val = self.LocateBlock( [0,3], sqrt( len( grid ) ) )
-	if val == 1:
-		failed = False
-	return failed
-	
-def FailedString( failed ):
-	if failed:
-		return "!!!! Failed !!!!"
-	else:
-		return "Passed"
-		
-def BigTest():
-	testResults = []
-	testResults.append( TestCheckArray() )
-	testResults.append( TestColToArray() )
-	testResults.append( TestCheckGridDimensions() )
-	testResults.append( TestBlockToArray() )
-	testResults.append( TestLocateNextEmpty() )
-	testResults.append( TestLocateBlock() )
-	
-	a = 0
-	for test in testResults:
-		print "Test %d: %s" % (a, FailedString( test ))
-		a = a + 1
-########################################
-
 
 #Find the most common value and choose to solve that next
 #Add pointers to the end of each row/col to maintain stacks of values possible
@@ -370,7 +247,7 @@ def BigTest():
 ########################################
 #				MAIN
 ########################################
-#BigTest()
+
 def main():
 	start = time.clock()
 	mySolver.FillGrid( grid, 0 )
